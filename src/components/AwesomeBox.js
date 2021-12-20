85f4bfefff72cad648cb10574ebe42bcf08dff71
unknown <pawel.wojtanowicz@gmail.com>
2021-12-20 22:06:29 +0100

    with rect as div

--- src/components/AwesomeBox.js
+++ src/components/AwesomeBox.js
@@ -1,8 +1,13 @@
 export function AwesomeComponent() {
     return (
-        <svg width="300" height="300">
+        /*<svg width="300" height="300">
         <rect  width="300" height="300" style={{fill:"red"}}/>
         <text x="150" y="150" text-anchor="middle" alignment-baseline="central" style={{fill:"white"}}>I'm AwesomeBox</text>
-        </svg>
+        </svg>*/
+        <div className="rect">
+            <div className="rectText">
+                Ich bin AwesomeBox
+            </div>
+        </div>
     )
 }
