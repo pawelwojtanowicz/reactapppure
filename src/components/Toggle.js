3047cf4f525550206ad0572aa4f7e694f61e6bbd
unknown <pawel.wojtanowicz@gmail.com>
2021-12-27 21:40:22 +0100

    after zad 8

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -4,11 +4,12 @@ export function Toggle(props) {
         
     const [display, setDisplay] = useState("display1");
     const [toggle, setToggle] = useState("toggle1");
+    const [toggleSecond, setToggleSecond] = useState("toggle2");
 
     return (
             <div>
-            <button className="toggle1" onClick={() => {setDisplay("display2")}}>Toggle</button>
-
+            <button className={ toggle } onClick={() => {setDisplay("display2"); setToggle("toggle2"); setToggleSecond("toggle1")}}>Toggle</button>
+            <button className={ toggleSecond } onClick={() => {setDisplay("display1"); setToggle("toggle1"); setToggleSecond("toggle2")}}>Toggle</button>
             <p className={ display }>Raz jestem, raz mnie nie ma</p>
             </div>
     )
